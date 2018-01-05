550ec39f48f822312e617cd1fe1b8455939b676e
root <leandrocamposcardoso0@gmail.com>
2018-01-05 15:51:41 +0000

    first

--- telegrambot.js
+++ telegrambot.js
@@ -134,11 +134,11 @@ module.exports = class TelegramBot {
                                     console.log(moeda)
                                     this.getCriptoCourrence(moeda, function (resp) {
                                         resp = JSON.parse(resp.replace(/]|[[]/g, ''))
-                                        var msg = "Valor: R$" + resp.price_brl.substring(0, str.length - 2);
-                                        console.log(msg)
+                                        var cripto_brl = "Valor: R$" + cripto_brl.price_brl.substring(0, str.length - 2);
+                                        console.log(cripto_brl)
                                         this.reply({
                                             chat_id: chatId,
-                                            text: msg
+                                            text: cripto_brl
                                         });
                                     })
                                     break;
