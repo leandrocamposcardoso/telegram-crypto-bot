e98d635d7d1542a46b09e6889ed2709961b15ecd
root <leandrocamposcardoso0@gmail.com>
2018-01-08 14:53:34 +0000

    first

--- telegrambot.js
+++ telegrambot.js
@@ -253,15 +253,15 @@ module.exports = class TelegramBot {
                 var keys = Object.keys(data);
                 for( var i = 0,length = keys.length; i < length; i++ ) {
                     obj_dic.push({
-                        'nome': data.Label,
-                        'pedido': data.AskPrice,
-                        'ofertado': data.BidPrice,
-                        'volume': data.Volume,
+                        'nome': data[i].Label,
+                        'pedido': data[i].AskPrice,
+                        'ofertado': data[i].BidPrice,
+                        'volume': data[i].Volume,
                         'variacao': (((
                                 parseFloat(
-                                    (data.AskPrice - 0.00000001).toFixed(8)
-                                ) - parseFloat((data.BidPrice + 0.00000001).toFixed(8))) /
-                            parseFloat((data.BidPrice + 0.00000001).toFixed(8))) * 100)
+                                    (data[i].AskPrice - 0.00000001).toFixed(8)
+                                ) - parseFloat((data[i].BidPrice + 0.00000001).toFixed(8))) /
+                            parseFloat((data[i].BidPrice + 0.00000001).toFixed(8))) * 100)
                     })
                 }
                 console.log(obj_dic)
